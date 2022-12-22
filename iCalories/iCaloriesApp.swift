785a92e8d65edc2d4b6f3137866c16c98fe45866
Philipp Sanktjohanser <venture@mein.gmx>
2022-12-22 18:40:12 +0100

    Initial Commit

---
+++ iCalories/iCaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCaloriesApp.swift
+//  iCalories
+//
+//  Created by Philipp Sanktjohanser on 22.12.22.
+//
+
+import SwiftUI
+
+@main
+struct iCaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
